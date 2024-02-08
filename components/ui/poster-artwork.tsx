c8e2f3bea1c391b85a5be6d4c540836e593f762b
Anthony Lacruz <lacruzanthony20@gmail.com>
2024-02-07 21:21:53 -0500

    ui(components): adding link navigation to the details

--- components/ui/poster-artwork.tsx
+++ components/ui/poster-artwork.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import { cn, imageUrl } from "@/lib/utils";
 import { movieDetails } from "@/services/movies";
-import { tvShowDetails } from "@/services/tv-shows";
+import { tvShowDetails } from "@/services/tv-shows/tv-show-details";
 import StarRating from "./start-rating";
+import Link from "next/link";
 
 interface AlbumArtworkProps extends React.HTMLAttributes<HTMLDivElement> {
   id: string;
@@ -11,6 +12,7 @@ interface AlbumArtworkProps extends React.HTMLAttributes<HTMLDivElement> {
   key?: string;
   width?: number;
   height?: number;
+  isSimilar?: boolean;
   isTvShow?: boolean;
   isSimilar?: boolean;
 }
@@ -23,34 +25,37 @@ export const PosterArtwork = async ({
   height,
   key,
   className,
+  isSimilar = false,
   isTvShow = false,
-  isSimilar,
   ...props
 }: AlbumArtworkProps) => {
   const { poster_path, title, name, vote_average } = isTvShow
     ? await tvShowDetails(id)
     : await movieDetails(id);
+  const href = isTvShow ? `show/${id}` : `movie/${id}`;
 
   return (
-    <div className={cn("space-y-3", className)} {...props}>
-      <div className="rounded-md max-w-xs m-2 overflow-hidden">
-        <Image
-          src={imageUrl(poster_path)}
-          alt={key ?? ""}
-          width={width}
-          height={height}
-          className={cn(
-            "h-auto w-auto object-cover transition-all hover:scale-105",
-            aspectRatio === "portrait" ? "aspect-[3/4]" : "aspect-square"
-          )}
-        />
+    <div className={cn("space-y-3 ", className)} {...props}>
+      <div className="rounded-md max-w-xs m-2 overflow-hidden ">
+        <Link href={href}>
+          <Image
+            src={imageUrl(poster_path)}
+            alt={key ?? ""}
+            width={width}
+            height={height}
+            className={cn(
+              "h-auto w-auto object-cover transition-all hover:scale-105",
+              aspectRatio === "portrait" ? "aspect-[3/4]" : "aspect-square"
+            )}
+          />
+        </Link>
       </div>
       <div className="space-y-1 text-sm">
-        <h3 className="font-medium leading-none">{isTvShow ? name : title}</h3>
-        <p className="text-xs text-muted-foreground">
-          People votes:
-          <StarRating isSimilar={isSimilar} average={vote_average} />
-        </p>
+        <h3 className="font-medium leading-none truncate">
+          {isTvShow ? name : title}
+        </h3>
+        <p className="text-xs text-muted-foreground">People votes:</p>
+        <StarRating average={vote_average} />
       </div>
     </div>
   );
--- components/ui/start-rating.tsx
+++ components/ui/start-rating.tsx
@@ -2,10 +2,10 @@ import { Fragment } from "react";
 
 const StarRating = ({
   average,
-  isSimilar,
+  isSimilar = false,
 }: {
-  average: number;
   isSimilar?: boolean;
+  average: number;
 }) => {
   const scaledAverage = average / 2;
   const roundedAverage = Math.round(scaledAverage * 2) / 2;
@@ -17,24 +17,25 @@ const StarRating = ({
         const filledStar = roundedAverage >= starNumber;
         const halfStar =
           roundedAverage > starNumber - 0.5 && roundedAverage < starNumber;
-        console.log({ filledStar });
 
         return (
           <Fragment key={index}>
-            <svg
-              className={`w-4 h-4 me-1 ${
-                filledStar ? "text-yellow-300" : "text-gray-300"
-              }`}
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
+            {!halfStar && (
+              <svg
+                className={`w-4 h-4 me-1 ${
+                  filledStar ? "text-yellow-300" : "text-gray-300"
+                }`}
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 22 20"
+              >
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+              </svg>
+            )}
             {halfStar && (
               <svg
-                className="h-5 w-5 fill-current text-yellow-400"
+                className="h-5 w-5 fill-current text-red-400"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
               >
