f6011e46285bcc0b124d3bfec297a0ca67f99c53
Anthony Lacruz <lacruzanthony20@gmail.com>
2024-02-14 21:58:35 -0500

    UI(component): blend depending on the background

--- components/ui/film-detail.tsx
+++ components/ui/film-detail.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { movieSlice, tvShowsSlice } from "@/lib/redux";
 import Image from "next/image";
-import { useDispatch } from "react-redux";
 
 export default function FilmDetail({
   title,
@@ -15,9 +13,12 @@ export default function FilmDetail({
   backdrop: string;
   poster: string;
 }) {
+  const textBlend = backdrop.includes("fallback")
+    ? "mix-blend-difference"
+    : "mix-blend-color-dodge";
   return (
     <div
-      className="p-8 relative h-90 bg-cover bg-no-repeat bg-center rounded-lg shadow-lg bg-[#d4d4d8]"
+      className="p-8 relative h-90 bg-cover bg-no-repeat bg-center rounded-lg shadow-lg bg-[#d4d4d8] bg-blend-luminosity"
       style={{
         backgroundImage: `url(${backdrop})`,
         objectFit: "cover",
@@ -31,7 +32,9 @@ export default function FilmDetail({
           src={poster}
           width="200"
         />
-        <div className="ml-8 text-white mix-blend-difference p-4 rounded-lg backdrop-blur-md backdrop-saturate-200">
+        <div
+          className={`ml-8 text-stone-200	 p-4 rounded-lg backdrop-blur-md backdrop-saturate-200 ${textBlend}`}
+        >
           <h1 className="text-4xl font-bold">{title}</h1>
           <h2 className="mt-4 text-2xl font-bold">Overview</h2>
           <p className="mt-2 text-lg">{overview}</p>
